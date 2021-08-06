1221bb1874dce4c7baed8b127b300df8c682ffdf
Frieder Olpen <frieder.olpen@valtech.com>
2021-08-06 17:06:02 +0200

    newly build lol

--- dist/index.js
+++ dist/index.js
@@ -2,9 +2,10 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 const plugin_1 = require("@parcel/plugin");
 const minimatch_1 = require("minimatch");
-function shouldSkip(filePath) {
-    const pkg = this.getPackage();
-    const configs = pkg?.externalsExcluder;
+const fs_extra_1 = require("fs-extra");
+const path_1 = require("path");
+function shouldSkip(filePath, projectRoot) {
+    const configs = fs_extra_1.readJsonSync(path_1.join(projectRoot, 'package.json')).externalsExcluder;
     if (!configs) {
         return;
     }
@@ -16,9 +17,17 @@ function shouldSkip(filePath) {
     return false;
 }
 exports.default = new plugin_1.Resolver({
-    async resolve({ filePath }) {
-        if (shouldSkip(filePath)) {
+    async resolve({ filePath, logger, options }) {
+        if (shouldSkip(filePath, options.projectRoot)) {
+            logger.verbose({
+                message: `✅ Skipping for ${filePath}`,
+            });
             return null;
+        }
+        else {
+            logger.verbose({
+                message: `❌ Not skipping for ${filePath}`,
+            });
         }
         return {
             filePath,
